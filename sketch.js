e056bcace92b58aece57fc68d6fe7ef86e3f2902
lathikaMohan <73482831+lathikaMohan@users.noreply.github.com>
2020-11-17 17:09:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+/*var fixedRect, movingRect;
+
+var gameObject1,gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(100,100,80,80);
+  gameObject2 = createSprite(250,100,80,80);
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+ if( isTouching(movingRect,gameObject2)){
+movingRect.shapeColor = "red";
+gameObject2.shapeColor = "red";
+
+ }
+ else{
+   movingRect.shapeColor = "green";
+   gameObject2/*.shapeColor = "green";
+ }
+
+
+
+  drawSprites();
+}
+//sprite.isTouching(target)
+function isTouching(object1,object2){
+
+
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+
+  return true;
+}
+else {
+
+  return false;
+}
+
+}
+*/
+
+
+var fixedRect, movingRect;
+var gameObject1;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  gameObject1 = createSprite(400, 100,80,30)
+
+
+  movingRect.velocityY = -5;
+ //fixedRect.velocityY = +5;
+  gameObject1.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  bounceOff(movingRect,gameObject1);
+
+
+  drawSprites();
+}
+
